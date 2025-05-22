3909993cee7051204de0b95ea1e6b00ed48882e3
IvanPro91 <ivan-don4encko@yandex.ru>
2025-05-22 16:37:19 +0300

    Выполненный проект на Django

--- catalog/views.py
+++ catalog/views.py
@@ -1,15 +1,7 @@
-from django.shortcuts import render, get_object_or_404
-
-from catalog.models import Product
-
+from django.shortcuts import render
 
 def view_page_home(request):
-    products = Product.objects.all()
-    return render(request, "home.html", {"products": products})
+    return render(request, "home.html")
 
 def view_page_contacts(request):
     return render(request, "contacts.html")
-
-def view_detail_product(request, id_product):
-    product = get_object_or_404(Product, pk=id_product)
-    return render(request, "details_product.html", {"product": product})
