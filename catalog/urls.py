547f48cb5bf9468345f0a6aa99bf6d6af7636a6b
IvanPro91 <ivan-don4encko@yandex.ru>
2025-05-23 18:42:49 +0300

    Исправление путей path

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,11 +1,13 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 
 from catalog.views import view_page_home, view_page_contacts
 
-urlpatterns = [
-    path("contacts/", view_page_contacts),
-    path("home/", view_page_home),
-    path("/", view_page_home),
-    path("", view_page_home),
-]
+app_name = 'catalog'
 
+urlpatterns = [
+    path("contacts/", view_page_contacts, name='contacts'),
+    path("", view_page_home, name='home'),
+]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
