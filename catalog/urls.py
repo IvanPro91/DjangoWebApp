afd388494811570e976c43bddabec339bef2e0c5
IvanPro91 <ivan-don4encko@yandex.ru>
2025-05-26 09:28:00 +0300

    Шаблонизация в django

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,11 +1,14 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 
-from catalog.views import view_page_home, view_page_contacts
+from catalog.views import view_page_home, view_page_contacts, view_detail_product
+
+app_name = 'catalog'
 
 urlpatterns = [
-    path("contacts/", view_page_contacts),
-    path("home/", view_page_home),
-    path("/", view_page_home),
-    path("", view_page_home),
+    path("contacts/", view_page_contacts, name='contacts'),
+    path("", view_page_home, name='home'),
+    path("details/<int:id_product>", view_detail_product, name='details_product'),
 ]
-
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- catalog/views.py
+++ catalog/views.py
@@ -1,7 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from catalog.models import Product
+
 
 def view_page_home(request):
-    return render(request, "home.html")
+    products = Product.objects.all()
+    return render(request, "home.html", {"products": products})
 
 def view_page_contacts(request):
     return render(request, "contacts.html")
+
+def view_detail_product(request, id_product):
+    product = get_object_or_404(Product, pk=id_product)
+    return render(request, "details_product.html", {"product": product})
