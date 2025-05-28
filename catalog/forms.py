8fce02022df4eb80fe653eb9828eea4d56b45f1a
IvanPro91 <ivan-don4encko@yandex.ru>
2025-05-28 12:16:25 +0300

    Добавление форм, валидации и стилизации элементов форм

---
+++ catalog/forms.py
@@ -0,0 +1,45 @@
+from django.forms import ModelForm, forms
+
+from catalog.models import Product
+
+
+class ProductForm(ModelForm):
+    class Meta:
+        model = Product
+        fields = "__all__"
+
+    def __init__(self, *args, **kwargs):
+        super(ProductForm, self).__init__(*args, **kwargs)
+
+        self.fields['name'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'Введите наименование товара'
+        })
+
+        self.fields['description'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'Введите описание'
+        })
+
+        self.fields['category'].widget.attrs.update({
+            'class': 'form-select',
+        })
+
+        self.fields['price'].widget.attrs.update({
+            'class': 'form-control',
+            'type': 'numeric'
+        })
+
+    def clean_price(self):
+        price = self.cleaned_data.get('price')
+        if price is not None:
+            if price < 0:
+                self.add_error('price', f'Цена не может быть меньше 0')
+        return price
+
+    def clean(self):
+        cleaned_data = super().clean()
+        name = cleaned_data.get('name')
+        word_valid = ["казино", "криптовалюта", "крипта", "биржа", "дешево", "бесплатно", "обман", "полиция", "радар"]
+        if name and name in word_valid:
+            self.add_error('name', f'Name не может содержать слово "{name}"')
--- catalog/urls.py
+++ catalog/urls.py
@@ -2,13 +2,17 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
 
-from catalog.views import ContactPageView, HomePageView, ProductDetailView
+from catalog.views import ContactPageView, HomePageView, ProductDetailView, ProductCreateView, ProductUpdateView, \
+    ProductDeleteView
 
 app_name = 'catalog'
 
 urlpatterns = [
     path("contacts/", ContactPageView.as_view(), name='contacts'),
     path("", HomePageView.as_view(), name='home'),
+    path("create_product/", ProductCreateView.as_view(), name='create_product'),
+    path("update_product/<int:pk>/update/", ProductUpdateView.as_view(), name='update_product'),
+    path("delete_product/<int:pk>/delete/", ProductDeleteView.as_view(), name='delete_product'),
     path("details/<int:pk>", ProductDetailView.as_view(), name='details_product'),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
